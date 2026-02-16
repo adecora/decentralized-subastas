085b0d3c7b753dd2d54ecd0e02656bf249ed9efa
Alejandro de Cora <alejandrodecora@gmail.com>
2026-02-16 12:44:33 +0100

    Actualizando el contrato

--- contracts/AuctionSystem.sol
+++ contracts/AuctionSystem.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.10;
 
 contract AuctionSystem {
 
+    enum AuctionStatus {
+      Active,    // 0 - Subasta activa
+      Completed, // 1 - Entrega confirmada por el ganador
+      Withdraw   // 2 - Cobrada por el creador
+    }
+
     struct Auction {
         uint256 auctionId;
         address creator;
@@ -11,6 +17,7 @@ contract AuctionSystem {
         uint256 deadline;
         address highestBidder;
         uint256 highestBid;
+        AuctionStatus status;
     }
 
     uint256 public auctionCount;
@@ -20,27 +27,27 @@ contract AuctionSystem {
     // Mapping con las pujas de los usuarios para cada subasta
     mapping(uint256 => mapping(address => uint256)) public bids;
 
-
-    uint256 private constant maxTimeToLive = 10080 minutes; // 1 semana = 60*24*7
-    uint256 private constant minTimeToLive = 1 minutes;
-    uint256 private constant minBid = 0.01 ether;
+    uint256 private constant MAX_TIMETOLIVE = 10080 minutes; // 1 semana = 60*24*7
+    uint256 private constant MIN_TIMETOLIVE = 1 minutes;
+    uint256 private constant MIN_BID = 0.01 ether;
 
     event AuctionCreated(uint256 auctionId, address creator, string description, uint256 deadline);
     event Bid(uint256 auctionId, address bidder, uint256 amount, uint256 timestamp);
+    event Refund(uint256 auctionId, address bidder, uint256 refundAmount);
+    event Receipt(uint256 auctionId, address winner);
+    event Withdraw(uint256 auctionId, address creator,uint256 withdrawAmount);
 
-    constructor() {
-
-    }
+    constructor() {}
 
     function createAuction(string memory _description, uint256 timeToLive) external returns(uint256) {
         // El tiempo recibido llega en minutos
         uint256 ttl = timeToLive * 1 minutes;
 
-        require(ttl >= minTimeToLive && ttl <= maxTimeToLive, "El tiempo de subasta no es validado");
+        require(ttl >= MIN_TIMETOLIVE && ttl <= MAX_TIMETOLIVE, "El tiempo de subasta no es validado");
         require(bytes(_description).length > 0, "La descripcion no puede estar vacia");
 
         ++auctionCount;
-        auctions[auctionCount] = Auction(auctionCount, msg.sender, _description, block.timestamp + ttl, address(0), 0);
+        auctions[auctionCount] = Auction(auctionCount, msg.sender, _description, block.timestamp + ttl, address(0), 0, AuctionStatus.Active);
 
         Auction storage auction = auctions[auctionCount];
 
@@ -68,7 +75,7 @@ contract AuctionSystem {
         Auction storage auction = auctions[_auctionId];
 
         require(_auctionId > 0 && _auctionId <= auctionCount, "La subasta no existe.");
-        require(msg.value >= minBid, "La puja tiene que ser de al menos 0.01 ether.");
+        require(msg.value >= MIN_BID, "La puja tiene que ser de al menos 0.01 ether.");
         require(bids[_auctionId][msg.sender] == 0, "No puedes volver a pujar.");
         require(block.timestamp < auction.deadline, "La subasta ya ha terminado.");
         require(msg.value > auction.highestBid, "La puja tiene que ser mayor que la puja actual.");
@@ -76,9 +83,6 @@ contract AuctionSystem {
         auction.highestBidder = msg.sender;
         auction.highestBid = msg.value;
         bids[_auctionId][msg.sender] = msg.value;
-
-        (bool success, ) =  payable(auction.highestBidder).call{value: auction.highestBid}("");
-        require(success, "Transfer failed");
 
         emit Bid(_auctionId, msg.sender, msg.value, block.timestamp);
     }
@@ -92,7 +96,6 @@ contract AuctionSystem {
         return auction.highestBidder;
     }
 
-
     function refund(uint256 _auctionId) external returns(uint256) {
         Auction storage auction = auctions[_auctionId];
 
@@ -105,8 +108,44 @@ contract AuctionSystem {
         (bool success, ) = payable(msg.sender).call{value: refundAmount}("");
         require(success, "La transferencia ha fallado");
 
+        emit Refund(_auctionId, msg.sender, refundAmount);
         // No pueda volver a solicitar la devolución de la puja
         bids[_auctionId][msg.sender] = 0;
         return refundAmount;
     }
+
+    function receipt(uint256 _auctionId) external {
+        Auction storage auction = auctions[_auctionId];
+
+        require(_auctionId > 0 && _auctionId <= auctionCount, "La subasta no existe.");
+        require(block.timestamp > auction.deadline, "La subasta no ha terminado.");
+        require(auction.status < AuctionStatus.Completed, "La entrega ya ha sido confirmada");
+        require(auction.highestBidder == msg.sender, "Solo la puja ganadora puede confirmar la entrega del articulo");
+
+        auction.status = AuctionStatus.Completed;
+        emit Receipt(_auctionId, msg.sender);
+    }
+
+    function auctionWithdraw(uint256 _auctionId) external returns (uint256) {
+        Auction storage auction = auctions[_auctionId];
+
+        require(_auctionId > 0 && _auctionId <= auctionCount, "La subasta no existe.");
+        require(block.timestamp > auction.deadline, "La subasta no ha terminado.");
+        require(auction.creator == msg.sender, "Solo el creador de la subasta puede cobrarla");
+        require(auction.status != AuctionStatus.Withdraw, "Ya se cobro la subasta.");
+        require(auction.status == AuctionStatus.Completed, "El comprador no ha confirmado la entrega del articulo");
+
+        uint256 withdrawAmount = auction.highestBid;
+        require(withdrawAmount > 0, "No hay fondos para retirar.");
+
+        (bool success, ) = payable(msg.sender).call{value: withdrawAmount}("");
+        require(success, "La transferencia ha fallado");
+
+        emit Withdraw(_auctionId, msg.sender, withdrawAmount);
+
+        // Ya's cobrao
+        auction.status = AuctionStatus.Withdraw;
+        return withdrawAmount;
+    }
+
 }
